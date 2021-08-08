e04b6984942b0e23bbce0277e59a52160fb53782
ignored-comment <info.konham@gmail.com>
2021-08-08 15:54:18 -0400

    Create weather service test; tests passing

---
+++ app/services/weather_service.rb
@@ -0,0 +1,18 @@
+class WeatherService
+    class << self
+        def get_weather(lat, lng)
+            response = conn.get("/data/2.5/onecall?lat=#{lat}&lon=#{lng}&units=imperial&exclude=minutely,alerts&appid=#{ENV['WEATHER_API']}")
+            parse_json(response)
+        end
+
+        private
+
+        def conn
+            Faraday.new('https://api.openweathermap.org')
+        end
+
+        def parse_json(response)
+            JSON.parse(response.body, symbolize_names: true)
+        end
+    end
+end
